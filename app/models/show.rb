9fe7d2c080394b0779445747dc1a47b277b10591
Paul Schreiber <paulschreiber@gmail.com>
2015-07-03 22:42:20 -0400

    add relationships to models

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,2 +1,5 @@
 class Show < ActiveRecord::Base
+  has_and_belongs_to_many :artists
+  has_many :rsvps
+  belongs_to :venue
 end
--- app/models/venue.rb
+++ app/models/venue.rb
@@ -1,2 +1,4 @@
 class Venue < ActiveRecord::Base
+  has_and_belongs_to_many :venue_groups
+  has_many :shows
 end
