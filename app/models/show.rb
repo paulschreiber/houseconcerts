99b7c165e0f304e5d657e3d75fe4da21d085fbb4
Paul Schreiber <paulschreiber@gmail.com>
2015-07-04 01:07:38 -0400

    add :upcoming and :confirmed scopes to shows model

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,5 +1,10 @@
 class Show < ActiveRecord::Base
+  STATUS_CONFIRMED = 'G'
+
   has_and_belongs_to_many :artists
   has_many :rsvps
   belongs_to :venue
+
+  scope :confirmed, -> { where(status: STATUS_CONFIRMED) }
+  scope :upcoming, -> { where("start > ?", Time.now) }
 end
