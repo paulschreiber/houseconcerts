b87fa38b06d14e47f4ecef3c86681fe4058bb9d6
Paul Schreiber <paulschreiber@gmail.com>
2015-07-05 12:05:02 -0400

    helpers for social media title/image/description + icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,31 @@
 module ApplicationHelper
   def page_title
-    page_title = [HC_CONFIG.page_title]
+    page_title = [HC_CONFIG.site_name]
     page_title.join(" » ")
+  end
+
+  def social_media_title
+    full_title = page_title
+    if full_title.index(" » ")
+      full_title.gsub("#{HC_CONFIG.site_name} » ", "")
+    else
+      full_title
+    end
+  end
+
+  def social_media_description
+    HC_CONFIG.meta_description
+  end
+
+  def social_media_image
+    "#{root_url}concerts.png"
+  end
+
+  def svg_icon
+    "#{root_url}concerts.svg"
+  end
+
+  def apple_touch_icon
+    "#{root_url}concerts.png"
   end
 end
