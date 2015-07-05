5951aae842d39f774535f4fab5048e957f9c6eea
Paul Schreiber <paulschreiber@gmail.com>
2015-07-05 11:58:05 -0400

    add facebook_app_secret to HC_CONFIG

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,1 +1,4 @@
+app_keys = YAML.load_file("#{Rails.root}/config/application.yml")
+
 HC_CONFIG = YAML.load_file("#{Rails.root}/config/settings.yml")
+HC_CONFIG.facebook_app_secret = app_keys.facebook_app_secret
