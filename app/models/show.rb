a432967d5036180bc051d8bedb4b791bf3877889
Paul Schreiber <paulschreiber@gmail.com>
2015-07-04 22:12:06 -0400

    have show scopes use config system

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,10 +1,8 @@
 class Show < ActiveRecord::Base
-  STATUS_CONFIRMED = 'G'
-
   has_and_belongs_to_many :artists
   has_many :rsvps
   belongs_to :venue
 
-  scope :confirmed, -> { where(status: STATUS_CONFIRMED) }
+  scope :confirmed, -> { where(status: HC_CONFIG.show.status[:confirmed]) }
   scope :upcoming, -> { where("start > ?", Time.now) }
 end
