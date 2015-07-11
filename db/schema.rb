652d48b9ed15eed5ab2851d1cb04d8f136f26cde
Paul Schreiber <paulschreiber@gmail.com>
2015-07-11 13:27:35 -0400

    update schema.db for friendlyid

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150705223439) do
+ActiveRecord::Schema.define(version: 20150711170112) do
 
   create_table "admins", force: :cascade do |t|
     t.string   "email",                  limit: 255, default: "", null: false
@@ -49,6 +49,19 @@ ActiveRecord::Schema.define(version: 20150705223439) do
 
   add_index "artists_shows", ["artist_id", "show_id"], name: "index_artists_shows_on_artist_id_and_show_id", using: :btree
   add_index "artists_shows", ["show_id", "artist_id"], name: "index_artists_shows_on_show_id_and_artist_id", using: :btree
+
+  create_table "friendly_id_slugs", force: :cascade do |t|
+    t.string   "slug",           limit: 255, null: false
+    t.integer  "sluggable_id",   limit: 4,   null: false
+    t.string   "sluggable_type", limit: 50
+    t.string   "scope",          limit: 255
+    t.datetime "created_at"
+  end
+
+  add_index "friendly_id_slugs", ["slug", "sluggable_type", "scope"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type_and_scope", unique: true, using: :btree
+  add_index "friendly_id_slugs", ["slug", "sluggable_type"], name: "index_friendly_id_slugs_on_slug_and_sluggable_type", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_id"], name: "index_friendly_id_slugs_on_sluggable_id", using: :btree
+  add_index "friendly_id_slugs", ["sluggable_type"], name: "index_friendly_id_slugs_on_sluggable_type", using: :btree
 
   create_table "opens", force: :cascade do |t|
     t.string   "tag",        limit: 255
