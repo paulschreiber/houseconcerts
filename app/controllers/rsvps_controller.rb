34999b30a24e5770d33364b3695728e1ad00a3de
Paul Schreiber <paulschreiber@gmail.com>
2015-07-12 18:51:33 -0400

    RSVP controller: now create and update reservations

--- app/controllers/rsvps_controller.rb
+++ app/controllers/rsvps_controller.rb
@@ -1,4 +1,8 @@
-class RSVPController < ApplicationController
+class RsvpsController < ApplicationController
+  def index
+    redirect_to new_rsvp_path
+  end
+
   def new
     @rsvp = RSVP.new(params[:rsvp])
     begin
@@ -8,4 +12,32 @@ class RSVPController < ApplicationController
       @shows = Show.upcoming.confirmed
     end
   end
+
+  def create
+    # look for an existing reservation
+    if params[:rsvp] && params[:rsvp][:email] && params[:rsvp][:show_id]
+      @rsvp = RSVP.where(show_id: params[:rsvp][:show_id], email: params[:rsvp][:email]).first
+    end
+
+    # update an existing reservation
+    if @rsvp.nil?
+      @rsvp = RSVP.new(rsvp_params)
+
+    # create a new reservation
+    else
+      @rsvp.update(rsvp_params)
+    end
+
+    @show = Show.find(params[:rsvp][:show_id])
+    @shows = Show.upcoming.confirmed
+
+    if @rsvp.save
+      render 'thanks' and return
+    end
+  end
+
+  def rsvp_params
+    params.require(:rsvp).permit(:first_name, :last_name, :email, :show_id, :postcode, :response, :seats)
+  end
+
 end
