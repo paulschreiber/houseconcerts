230758bbc5bee5b64443667508f4f380c812c7a3
Paul Schreiber <paulschreiber@gmail.com>
2015-07-12 20:44:27 -0400

    fix IP address fetching/storing in person model

--- app/models/person.rb
+++ app/models/person.rb
@@ -11,26 +11,28 @@ class Person < ActiveRecord::Base
     rand(2821109907456).to_s(36)
   end
 
+  cattr_accessor :current_ip
+
   default_value_for :status, "active"
 
   validates :first_name, presence: true, mixed_case: true
   validates :last_name, presence: true, mixed_case: true
   validates :email, email: true
-  validates :postcode, postal_code: { country: HC_CONFIG.default_country }
+  validates :postcode, postal_code: { country: HC_CONFIG.default_country }, allow_blank: true
   validates :status, inclusion: { in: HC_CONFIG.person.status }
-
-  def set_ip_address
-    self.ip_address = request.remote_addr if ip_address.empty?
-  end
 
   def update_removal_status
     if self.status_changed? and self.status == "removed"
       self.removed_at = DateTime.now
-      self.removal_ip_address = request.remote_addr
+      self.removal_ip_address = current_ip
     end
   end
 
   def full_name
     "#{first_name} #{last_name}".strip
+  end
+
+  def set_ip_address
+    self.ip_address = current_ip if ip_address.nil?
   end
 end
