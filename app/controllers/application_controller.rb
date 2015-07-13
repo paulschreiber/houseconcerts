fa90bfbc909ea5f12022322b0cbbb6ad930a7441
Paul Schreiber <paulschreiber@gmail.com>
2015-07-12 20:43:44 -0400

    gra IP address on each request so we can use it in the model

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,11 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  before_filter :set_current_ip
+
+  protected
+  def set_current_ip
+    Person.current_ip = request.remote_addr
+  end
 end
