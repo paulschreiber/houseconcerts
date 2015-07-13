cd969c2ef8dc2d229aaa2bd9ca1082ea40c4fe7c
Paul Schreiber <paulschreiber@gmail.com>
2015-07-12 20:45:22 -0400

    add logic for mailing list controller

--- app/controllers/mailing_list_controller.rb
+++ app/controllers/mailing_list_controller.rb
@@ -1,6 +1,27 @@
 class MailingListController < ApplicationController
   def index
-    @person = Person.new(params[:person])
+    @person = Person.new
+  end
 
+  def create
+    # look for an existing reservation
+    if params[:person] && params[:person][:email]
+      @person = Person.where(email: params[:person][:email]).first
+    end
+
+    if @person.present?
+      render 'already_subscribed' and return
+    end
+
+    @person = Person.new(person_params)
+
+    if @person.save
+      render 'thanks' and return
+    end
+
+  end
+
+  def person_params
+    params.require(:person).permit(:first_name, :last_name, :email, :postcode)
   end
 end
