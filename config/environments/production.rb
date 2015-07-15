ad8c642c05e74dcdb4891f47c60f968ceca82145
Paul Schreiber <paulschreiber@gmail.com>
2015-07-15 00:42:24 -0400

    set default production mailer to https

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'concerts.shrub.ca', port: 443 }
+  config.action_mailer.default_url_options = { host: 'concerts.shrub.ca', protocol: 'https' }
 end
