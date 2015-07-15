0611c2e4d8c72b4b36a971384f8baad604baf258
Paul Schreiber <paulschreiber@gmail.com>
2015-07-15 01:04:47 -0400

    add Mandrill to invite mailer

--- app/mailers/invites.rb
+++ app/mailers/invites.rb
@@ -3,8 +3,20 @@ class Invites < ApplicationMailer
     @person = person
     @show = show
     @rsvp_url = url_for(controller: :rsvps, action: :new, slug: show.slug, uniqid: person.uniqid)
+    subject = "You’re invited: #{show.name} house concert (#{show.start_date_short})"
 
-    mail(to: person.email_address_with_name)
+    delivery_options = {
+      user_name: HC_CONFIG.mandrill_username,
+      password: HC_CONFIG.mandrill_api_key,
+      address: "smtp.mandrillapp.com"
+    }
+
+    puts delivery_options.inspect
+
+    mail(to: person.email_address_with_name,
+        subject: subject,
+        delivery_method: :smtp,
+        delivery_method_options: delivery_options)
 
   end
 end
