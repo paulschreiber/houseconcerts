1730f430a9719f1937c4e6ac32c6c93fce0ce2e2
Paul Schreiber <paul.schreiber@espn.com>
2015-07-15 19:13:02 -0400

    invite mailer now bails early when given bad input

--- app/mailers/invites.rb
+++ app/mailers/invites.rb
@@ -1,5 +1,7 @@
 class Invites < ApplicationMailer
   def invite(person, show)
+		return unless person && show
+
     @person = person
     @show = show
     @rsvp_url = url_for(controller: :rsvps, action: :new, slug: show.slug, uniqid: person.uniqid)
