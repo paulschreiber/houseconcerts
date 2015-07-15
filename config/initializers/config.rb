3b625990982186edec2adfa2c5a9dce74925c9ed
Paul Schreiber <paulschreiber@gmail.com>
2015-07-15 01:04:09 -0400

    add mandrill config to HC_CONFIG

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -2,3 +2,5 @@ app_keys = YAML.load_file("#{Rails.root}/config/application.yml")
 
 HC_CONFIG = YAML.load_file("#{Rails.root}/config/settings.yml")
 HC_CONFIG.facebook_app_secret = app_keys.facebook_app_secret
+HC_CONFIG.mandrill_username = app_keys.mandrill_username
+HC_CONFIG.mandrill_api_key = app_keys.mandrill_api_key
