9b472e3fb576f69c11270bdab4ae78e4ae9bfec4
Paul Schreiber <paulschreiber@gmail.com>
2015-07-17 21:57:00 -0400

    set social media description and image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -14,11 +14,25 @@ module ApplicationHelper
   end
 
   def social_media_description
-    HC_CONFIG.meta_description
+    if @show && !@show.artists.empty?
+      description = "RSVP for the #{@show.name} house concert"
+      description += " in #{@show.location}" if @show.location
+      description += " on #{@show.start_date}"
+      description
+    else
+      HC_CONFIG.meta_description
+    end
   end
 
   def social_media_image
-    Rails.public_path.join('concerts.png')
+    if @show && !@show.artists.empty?
+      return root_url + @show.artists.first.photo
+    elsif current_page?(root_url)
+      next_show = Show.upcoming.first
+      return root_url + next_show.artists.first.photo if next_show.present? && !next_show.artists.empty?
+    end
+
+    root_url + 'concerts.png'
   end
 
   def svg_icon
