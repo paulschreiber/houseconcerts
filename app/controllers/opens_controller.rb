3457f5ec2f57f91a326b8875a1c7275410eec6a9
Paul Schreiber <paulschreiber@gmail.com>
2015-07-22 01:08:10 -0400

    add Opens controller

---
+++ app/controllers/opens_controller.rb
@@ -0,0 +1,19 @@
+class OpensController < ApplicationController
+  def index
+    person = Person.where(uniqid: params[:uniqid]).first if params[:uniqid]
+    tag = params[:tag]
+
+    if person && tag
+      Open.create(tag: tag, email: person.email, ip_address: Person.current_ip, open: true)
+    end
+
+    send_blank_gif
+  end
+
+  def send_blank_gif
+    send_data(Base64.decode64("R0lGODlhAQABAPAAAAAAAAAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw=="),
+              type: 'image/gif',
+              disposition: 'inline')
+  end
+
+end
---
+++ test/controllers/opens_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class OpensControllerTest < ActionController::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
