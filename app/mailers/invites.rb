0a4d7003660012b9823a23d4371b940a7d08d35b
Paul Schreiber <paulschreiber@gmail.com>
2015-07-22 01:26:06 -0400

    add open tracking and sendmal support

--- app/mailers/invites.rb
+++ app/mailers/invites.rb
@@ -1,6 +1,18 @@
 class Invites < ApplicationMailer
-  def invite(person, show)
-    return unless person && show && person.is_a?(Person) && show.is_a?(Show)
+  USE_MANDRILL = false
+
+  def invite(person, show, email_type = 'invite')
+    return unless person && show
+
+    unless person.is_a?(Person)
+      logger.warn "First parameter is of type #{person.class} and must be of type Person"
+      return
+    end
+
+    unless show.is_a?(Show)
+      logger.warn "Second parameter is of type #{show.class} and must be of type Show"
+      return
+    end
 
     unless person.active?
       logger.warn "Cannnot email inactive person (#{person.email}, #{person.id}, #{person.status})"
@@ -10,14 +22,18 @@ class Invites < ApplicationMailer
     @person = person
 
     @show = show
+    tag = "#{show.slug}:#{email_type}"
     @rsvp_url = url_for(controller: :rsvps, action: :new, slug: show.slug, uniqid: person.uniqid)
     @unsub_url = url_for(controller: :mailing_list, action: :unsubscribe, uniqid: person.uniqid)
+    @track_url = url_for(controller: :opens, action: :index, uniqid: person.uniqid, tag: tag)
 
     subject = "You’re invited: #{show.name} house concert (#{show.start_date_short})"
 
-    if Rails.env.production?
+    logger.debug "Emailing #{person.email} [#{tag}]"
+
+    if Rails.env.production? && USE_MANDRILL
       @unsub_url = "*|UNSUB:#{@unsub_url}|*"
-      headers['X-MC-Tags'] = "invite #{show.slug}"
+      headers['X-MC-Tags'] = tag
 
       delivery_options = {
         user_name: HC_CONFIG.mandrill_username,
@@ -30,6 +46,10 @@ class Invites < ApplicationMailer
            subject: subject,
            delivery_method: :smtp,
            delivery_method_options: delivery_options)
+    elsif Rails.env.production?
+      mail(to: person.email_address_with_name,
+           subject: subject,
+           delivery_method: :sendmail)
     else
       mail(to: person.email_address_with_name, subject: subject)
     end
