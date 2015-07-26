50a7c3955af6950ebe77dff6ada0bcbcfba6787b
Paul Schreiber <paulschreiber@gmail.com>
2015-07-26 18:50:58 -0400

    add RSVP#email_address_with_name

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -61,4 +61,13 @@ class RSVP < ActiveRecord::Base
   def full_name
     "#{first_name} #{last_name}".strip
   end
+
+  def email_address_with_name
+    return unless email.present?
+    if full_name.present?
+      "\"#{full_name}\" <#{email}>"
+    else
+      email
+    end
+  end
 end
