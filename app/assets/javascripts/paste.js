2aca7ba1b42a2ecfcf13857b4847193dce7d05ff
Paul Schreiber <paul.schreiber@espn.com>
2015-11-11 16:19:57 -0500

    add support for pasting name+email to mailing list form

--- app/assets/javascripts/paste.js
+++ app/assets/javascripts/paste.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+
 	if ( $('#rsvp_first_name').length > 0 ) {
 		$('#rsvp_first_name').on( 'paste', function(event) {
 			var text = event.originalEvent.clipboardData.getData('text') ;
@@ -8,6 +9,19 @@ $(document).ready(function() {
 				$('#rsvp_first_name').val( matches[1] );
 				$('#rsvp_last_name').val( matches[2] );
 				$('#rsvp_email').val( matches[3] );
+			}
+		});
+	}
+
+	if ( $('#person_first_name').length > 0 ) {
+		$('#person_first_name').on( 'paste', function(event) {
+			var text = event.originalEvent.clipboardData.getData('text') ;
+			var matches = text.match( /(.*) (.*) <([^>]+)>/ );
+			if ( null !== matches ) {
+				event.preventDefault();
+				$('#person_first_name').val( matches[1] );
+				$('#person_last_name').val( matches[2] );
+				$('#person_email').val( matches[3] );
 			}
 		});
 	}
