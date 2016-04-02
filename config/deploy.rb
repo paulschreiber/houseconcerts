1dad19fde865876ff3d76ba5e712c80b1f19f14d
Paul Schreiber <paulschreiber@gmail.com>
2016-04-02 14:46:22 -0400

    share log directory

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :log_level, :info
 set :linked_files, %w{config/database.yml config/application.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
+set :linked_dirs, fetch(:linked_dirs, []).push('log', 'vendor/bundle')
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
