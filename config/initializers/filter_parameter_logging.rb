a378036f325fa59cb00ab1ee9e58a493099f9e4a
Paul Schreiber <paulschreiber@gmail.com>
2022-06-12 16:04:57 -0400

    update scripts for rails 6

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,7 +1,9 @@
 # Be sure to restart your server when you modify this file.
 
 # Configure sensitive parameters which will be filtered from the log file.
-Rails.application.config.filter_parameters += [:password]
+Rails.application.config.filter_parameters += [
+  :password, :passw, :secret, :token, :_key, :crypt, :salt, :certificate, :otp, :ssn
+]
 
 ActiveSupport::Inflector.inflections(:en) do |inflect|
   inflect.acronym 'RSVP'
