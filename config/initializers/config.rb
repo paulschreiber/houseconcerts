eddc55b9064e2534cdc491796aa517665507cb75
Paul Schreiber <paulschreiber@gmail.com>
2025-03-04 13:28:46 -0500

    fix: remove AWS SES config

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -7,9 +7,3 @@ HC_CONFIG.amazon_server = app_keys.amazon_server
 HC_CONFIG.twilio_account_sid = app_keys.twilio_account_sid
 HC_CONFIG.twilio_auth_token = app_keys.twilio_auth_token
 HC_CONFIG.twilio_sms_sender = app_keys.twilio_sms_sender
-
-ActionMailer::Base.add_delivery_method :ses, AWS::SES::Base,
-  :access_key_id     => HC_CONFIG.amazon_username,
-  :secret_access_key => HC_CONFIG.amazon_password,
-  :server            => HC_CONFIG.amazon_server,
-  :signature_version => 4
