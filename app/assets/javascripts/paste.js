1ca90ed59884a2bdad3fff8472a140acb75c196a
Paul Schreiber <paulschreiber@gmail.com>
2025-03-04 22:03:24 -0500

    style: reformat JS

--- app/assets/javascripts/paste.js
+++ app/assets/javascripts/paste.js
@@ -1,29 +1,27 @@
-$(document).ready(function() {
+$(document).ready(function () {
+  if ($("#rsvp_first_name").length > 0) {
+    $("#rsvp_first_name").on("paste", function (event) {
+      var text = event.originalEvent.clipboardData.getData("text");
+      var matches = text.match(/(.*) (.*) <([^>]+)>/);
+      if (null !== matches) {
+        event.preventDefault();
+        $("#rsvp_first_name").val(matches[1]);
+        $("#rsvp_last_name").val(matches[2]);
+        $("#rsvp_email").val(matches[3]);
+      }
+    });
+  }
 
-	if ( $('#rsvp_first_name').length > 0 ) {
-		$('#rsvp_first_name').on( 'paste', function(event) {
-			var text = event.originalEvent.clipboardData.getData('text') ;
-			var matches = text.match( /(.*) (.*) <([^>]+)>/ );
-			if ( null !== matches ) {
-				event.preventDefault();
-				$('#rsvp_first_name').val( matches[1] );
-				$('#rsvp_last_name').val( matches[2] );
-				$('#rsvp_email').val( matches[3] );
-			}
-		});
-	}
-
-	if ( $('#person_first_name').length > 0 ) {
-		$('#person_first_name').on( 'paste', function(event) {
-			var text = event.originalEvent.clipboardData.getData('text') ;
-			var matches = text.match( /(.*) (.*) <([^>]+)>/ );
-			if ( null !== matches ) {
-				event.preventDefault();
-				$('#person_first_name').val( matches[1] );
-				$('#person_last_name').val( matches[2] );
-				$('#person_email').val( matches[3] );
-			}
-		});
-	}
+  if ($("#person_first_name").length > 0) {
+    $("#person_first_name").on("paste", function (event) {
+      var text = event.originalEvent.clipboardData.getData("text");
+      var matches = text.match(/(.*) (.*) <([^>]+)>/);
+      if (null !== matches) {
+        event.preventDefault();
+        $("#person_first_name").val(matches[1]);
+        $("#person_last_name").val(matches[2]);
+        $("#person_email").val(matches[3]);
+      }
+    });
+  }
 });
-
