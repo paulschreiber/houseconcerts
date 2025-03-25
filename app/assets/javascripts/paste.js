80fe2e95f3ec1a00cd5b725e46ba038ee1c62572
Paul Schreiber <paulschreiber@gmail.com>
2025-03-24 22:13:50 -0400

    refactor: remove jQuery (#89)
    * chore: remove jquery gem and includes
    
    * refactor: rewrite JS without jQuery
    
    * chore: set JS max line length to 100

--- app/assets/javascripts/paste.js
+++ app/assets/javascripts/paste.js
@@ -1,26 +1,26 @@
-$(document).ready(function () {
-  if ($("#rsvp_first_name").length > 0) {
-    $("#rsvp_first_name").on("paste", function (event) {
-      var text = event.originalEvent.clipboardData.getData("text");
-      var matches = text.match(/(.*) (.*) <([^>]+)>/);
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.getElementById("rsvp_first_name")) {
+    document.getElementById("rsvp_first_name").addEventListener("paste", function (event) {
+      const text = event.clipboardData.getData("text");
+      const matches = text.match(/(.*) (.*) <([^>]+)>/);
       if (null !== matches) {
         event.preventDefault();
-        $("#rsvp_first_name").val(matches[1]);
-        $("#rsvp_last_name").val(matches[2]);
-        $("#rsvp_email").val(matches[3]);
+        document.getElementById("rsvp_first_name").value = matches[1];
+        document.getElementById("rsvp_last_name").value = matches[2];
+        document.getElementById("rsvp_email").value = matches[3];
       }
     });
   }
 
-  if ($("#person_first_name").length > 0) {
-    $("#person_first_name").on("paste", function (event) {
-      var text = event.originalEvent.clipboardData.getData("text");
-      var matches = text.match(/(.*) (.*) <([^>]+)>/);
+  if (document.getElementById("person_first_name")) {
+    document.getElementById("person_first_name").addEventListener("paste", function (event) {
+      const text = event.clipboardData.getData("text");
+      const matches = text.match(/(.*) (.*) <([^>]+)>/);
       if (null !== matches) {
         event.preventDefault();
-        $("#person_first_name").val(matches[1]);
-        $("#person_last_name").val(matches[2]);
-        $("#person_email").val(matches[3]);
+        document.getElementById("person_first_name").value = matches[1];
+        document.getElementById("person_last_name").value = matches[2];
+        document.getElementById("person_email").value = matches[3];
       }
     });
   }
