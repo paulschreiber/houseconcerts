48b2c02c4338eb3bc100fd5e8b9f9bd0c3896298
Paul Schreiber <paulschreiber@gmail.com>
2025-05-08 16:05:37 -0400

    fix: combine JS files

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,4 +24,18 @@ document.addEventListener("DOMContentLoaded", function () {
       }
     });
   }
+
+  document.querySelectorAll('input[name="rsvp[seats_reserved]"]').forEach((item) =>
+    item.addEventListener("click", function () {
+      document.getElementById("rsvp_response_yes").checked = true;
+    })
+  );
+
+  if (document.getElementById("rsvp_response_no")) {
+    document.getElementById("rsvp_response_no").addEventListener("click", function () {
+      document
+        .querySelectorAll('input[name="rsvp[seats_reserved]"]')
+        .forEach((item) => (item.checked = false));
+    });
+  }
 });
