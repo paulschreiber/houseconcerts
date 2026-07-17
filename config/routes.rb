53e7a4a8ab910aa06f47d1cf682b9899b3a9757b
Paul Schreiber <paulschreiber@gmail.com>
2026-07-17 14:14:34 -0400

    style: use to: keyword instead of hash-rocket route mapping
    get "path" => "controller#action" still works fine in Rails 8, just
    predates the to:/as: keyword style used elsewhere in this same file.
    Route table verified byte-identical before/after (bin/rails routes).

--- config/routes.rb
+++ config/routes.rb
@@ -7,13 +7,13 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root "shows#index"
-  get "about" => "shows#about", as: "about"
-  get "musicians" => "shows#musicians", as: "musicians"
-  get "shows" => "shows#shows", as: "past_shows"
-  get "privacy" => "privacy#index", as: "privacy"
-  get "list" => "mailing_list#index", as: "mailing_list"
-  get "unsubscribe/:uniqid" => "mailing_list#unsubscribe", as: "unsubscribe"
-  get "calendar/ical" => "shows#ical"
+  get "about", to: "shows#about", as: "about"
+  get "musicians", to: "shows#musicians", as: "musicians"
+  get "shows", to: "shows#shows", as: "past_shows"
+  get "privacy", to: "privacy#index", as: "privacy"
+  get "list", to: "mailing_list#index", as: "mailing_list"
+  get "unsubscribe/:uniqid", to: "mailing_list#unsubscribe", as: "unsubscribe"
+  get "calendar/ical", to: "shows#ical"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -24,16 +24,16 @@ Rails.application.routes.draw do
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
   resources :rsvps, only: %i[new index create]
-  get "rsvps" => "rsvps#new", as: "rsvp"
-  patch "rsvps" => "rsvps#create"
-  get "rsvps/show/:slug" => "rsvps#new", as: "rsvp_for_show"
-  get "rsvps/show/:slug/:uniqid" => "rsvps#new", as: "modify_rsvp"
-  get "rsvps/show/:slug/:uniqid/:response" => "rsvps#new", as: "rsvp_response"
-  post "rsvps/show/:slug" => "rsvps#new"
+  get "rsvps", to: "rsvps#new", as: "rsvp"
+  patch "rsvps", to: "rsvps#create"
+  get "rsvps/show/:slug", to: "rsvps#new", as: "rsvp_for_show"
+  get "rsvps/show/:slug/:uniqid", to: "rsvps#new", as: "modify_rsvp"
+  get "rsvps/show/:slug/:uniqid/:response", to: "rsvps#new", as: "rsvp_response"
+  post "rsvps/show/:slug", to: "rsvps#new"
 
-  post "sms" => "text_messages#receive"
+  post "sms", to: "text_messages#receive"
 
-  get "open/:tag/:uniqid" => "opens#index", as: "open_tracking"
+  get "open/:tag/:uniqid", to: "opens#index", as: "open_tracking"
 
   resources :people, only: %i[new index create], controller: :mailing_list
 end
