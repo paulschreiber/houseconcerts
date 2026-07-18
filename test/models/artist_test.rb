db6e11818708b6d742d269414aab4e482fdf4d19
Paul Schreiber <paulschreiber@gmail.com>
2026-07-17 22:36:21 -0400

    test: add model coverage for Artist and VenueGroup
    Artist: validations (name presence, url format), photo path derived
    from slug, shows association through artist_shows.
    
    VenueGroup: validations, venues/people associations through their
    respective join models, dependent: :destroy cleanup of join records.
    
    No bugs found this time - both models already behaved correctly.

--- test/models/artist_test.rb
+++ test/models/artist_test.rb
@@ -1,7 +1,36 @@
 require "test_helper"
 
 class ArtistTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "fixture is valid" do
+    assert artists(:one).valid?
+  end
+
+  test "requires a name" do
+    artist = Artist.new(artists(:one).attributes.except("id").merge("name" => ""))
+    assert_not artist.valid?
+    assert_includes artist.errors.attribute_names, :name
+  end
+
+  test "requires a valid url" do
+    artist = Artist.new(artists(:one).attributes.except("id").merge("url" => "not a url"))
+    assert_not artist.valid?
+    assert_includes artist.errors.attribute_names, :url
+  end
+
+  test "photo is derived from the slug" do
+    assert_equal "photos/headshots/#{artists(:one).slug}.jpg", artists(:one).photo
+  end
+
+  test "shows association resolves through artist_shows" do
+    assert_includes artists(:one).shows, shows(:upcoming)
+  end
+
+  test "adding a show creates the join record" do
+    artist = Artist.create!(name: "New Artist", url: "https://example.com/new-artist")
+
+    assert_difference("ArtistShow.count", 1) do
+      artist.shows << shows(:past)
+    end
+    assert_includes artist.shows, shows(:past)
+  end
 end
