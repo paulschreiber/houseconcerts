938f8b4410aa797e03b7cae7c48f5b29a612a0b8
Paul Schreiber <paulschreiber@gmail.com>
2026-08-17 00:15:42 -0400

    feat: add RSVP#person helper

--- app/models/rsvp.rb
+++ app/models/rsvp.rb
@@ -97,6 +97,10 @@ class RSVP < ApplicationRecord
     person&.update(phone_number: phone_number)
   end
 
+  def person
+    Person.find_by(email: email)
+  end
+
   # notify_rsvp can be "yes", "all" (yes and no) or blank/false/empty string
   def notify_admin
     # don't notify of any RSVPs when notify is empty
