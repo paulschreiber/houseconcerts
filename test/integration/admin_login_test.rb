3ae184bef4f951d7c0cc64e9ae90b64d99e4d29e
Paul Schreiber <paulschreiber@gmail.com>
2026-08-25 00:37:14 -0400

    test: cover password reset and trackable sign-in stats
    Adds coverage for the recoverable (forgot/reset password) flow and for the sign_in_count/current_sign_in_at/last_sign_in_at fields that trackable updates on login.

--- test/integration/admin_login_test.rb
+++ test/integration/admin_login_test.rb
@@ -60,4 +60,47 @@ class AdminLoginTest < ActionDispatch::IntegrationTest
 
     assert_nil cookies["remember_admin_token"]
   end
+
+  test "resets a forgotten password and can sign in with the new one" do
+    admin = admins(:one)
+
+    assert_emails 1 do
+      post admin_password_path, params: { admin: { email: admin.email } }
+    end
+    assert_redirected_to new_admin_session_path
+
+    raw_token = ActionMailer::Base.deliveries.last.body.encoded[/reset_password_token=([^"&\s]+)/, 1]
+    assert raw_token.present?
+
+    patch admin_password_path, params: {
+      admin: {
+        reset_password_token: raw_token,
+        password: "a brand new password",
+        password_confirmation: "a brand new password"
+      }
+    }
+    assert_redirected_to root_path
+
+    delete destroy_admin_session_path
+
+    post admin_session_path, params: {
+      admin: { email: admin.email, password: "a brand new password" }
+    }
+    assert_redirected_to root_path
+  end
+
+  test "tracks sign in count and timestamps" do
+    admin = admins(:one)
+    assert_equal 0, admin.sign_in_count
+    assert_nil admin.current_sign_in_at
+
+    post admin_session_path, params: {
+      admin: { email: admin.email, password: "correct horse battery staple" }
+    }
+
+    admin.reload
+    assert_equal 1, admin.sign_in_count
+    assert_not_nil admin.current_sign_in_at
+    assert_not_nil admin.last_sign_in_at
+  end
 end
