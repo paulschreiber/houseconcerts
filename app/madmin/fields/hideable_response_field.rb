2b2c390953efd215ead8623fd3749add5a078b29
Paul Schreiber <paulschreiber@gmail.com>
2026-08-26 13:53:48 -0400

    fix: hide response column for next/previous show attendees views

---
+++ app/madmin/fields/hideable_response_field.rb
@@ -0,0 +1,10 @@
+# response is hidden as an index column on the RSVPs "next_show_attendees"
+# scope (set via Current.admin_scope in Madmin::RsvpsController), since
+# that scope only ever includes "yes" responses.
+class HideableResponseField < RadioEnumField
+  def visible?(action)
+    return false if action.to_sym == :index && %w[next_show_attendees previous_show_attendees].include?(Current.admin_scope)
+
+    super
+  end
+end
