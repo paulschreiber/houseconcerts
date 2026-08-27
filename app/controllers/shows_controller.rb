e3aea7125936224f1124fb430ab9bd5c3b868e6a
Paul Schreiber <paulschreiber@gmail.com>
2026-08-27 16:18:56 -0400

    feat: add calendar feed

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -8,6 +8,18 @@ class ShowsController < ApplicationController
   end
 
   def calendar
-    head :ok
+    cal = Icalendar::Calendar.new
+    Show.upcoming.each do |show|
+      cal.event do |e|
+        e.dtstart     = Icalendar::Values::DateTime.new(show.start)
+        e.dtend       = Icalendar::Values::DateTime.new(show.end)
+        e.summary     = "#{show.name} House Concert"
+        e.location    = show.location
+        e.url         = rsvp_for_show_url(slug: show.slug)
+        e.uid         = "event-#{show.slug}@#{Settings.domain}"
+      end
+    end
+    cal.publish
+    render plain: cal.to_ical, content_type: "text/calendar"
   end
 end
