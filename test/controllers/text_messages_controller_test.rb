8b9bc56b4ea0c6d0063f792df67a10424a4aa2c2
Paul Schreiber <paulschreiber@gmail.com>
2026-08-27 17:23:23 -0400

    test: test twilio signature

--- test/controllers/text_messages_controller_test.rb
+++ test/controllers/text_messages_controller_test.rb
@@ -1,11 +1,28 @@
 require "test_helper"
 
 class TextMessagesControllerTest < ActionDispatch::IntegrationTest
-  test "receive delivers a notification email" do
+  test "receive delivers a notification email when the signature is valid" do
+    body = { "From" => "+15551234567", "Body" => "hello" }
+
     assert_emails 1 do
-      post sms_url, params: { From: "+15551234567", Body: "hello" }
+      post sms_url, params: body, headers: { "X-Twilio-Signature" => twilio_signature_for(sms_url, body) }
     end
 
     assert_response :success
   end
+
+  test "receive rejects requests without a valid Twilio signature" do
+    assert_no_emails do
+      post sms_url, params: { From: "+15551234567", Body: "hello" }
+    end
+
+    assert_response :forbidden
+  end
+
+  private
+
+    def twilio_signature_for(url, params)
+      validator = Twilio::Security::RequestValidator.new(Rails.application.credentials.twilio[:auth_token])
+      validator.build_signature_for(url, params)
+    end
 end
