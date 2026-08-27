7a8c73b7d361e9f0750d670dbb1d30f0fb2c6864
Paul Schreiber <paulschreiber@gmail.com>
2026-08-27 17:23:01 -0400

    fix: verify twilio signature on inbound SMS

--- app/controllers/text_messages_controller.rb
+++ app/controllers/text_messages_controller.rb
@@ -1,7 +1,19 @@
 class TextMessagesController < ApplicationController
   skip_before_action :verify_authenticity_token
+  before_action :verify_twilio_signature!
 
   def receive
     NotifyMailer.text_message(params["From"], params["Body"]).deliver_now
   end
+
+  private
+
+    def verify_twilio_signature!
+      signature = request.headers["X-Twilio-Signature"]
+      validator = Twilio::Security::RequestValidator.new(Rails.application.credentials.twilio[:auth_token])
+
+      return if signature.present? && validator.validate(request.original_url, request.request_parameters, signature)
+
+      head :forbidden
+    end
 end
